9df855d5541b01d6a2b68da36fab67571df88395
RonconiLudovico <ronconi.ludovico@gmail.com>
2025-03-06 14:05:58 +0100

    created new sample files created new script file

---
+++ script.py
@@ -0,0 +1,52 @@
+import sys
+
+def read_files(files):
+    for file in files:
+        with open(file, 'r') as new_f:
+            lines = new_f.readlines()
+            for line in lines:
+                return line
+
+
+def concatenate_files(files):
+    new_file = "new_file.txt"
+    with open(new_file, 'w') as f_new:
+        for i in files:
+            with open(i, 'r') as f:
+                lines = f.readlines()
+
+                for line in lines:
+                    f_new.write(line)
+    
+
+def clean_dataset():
+    pass
+
+            
+
+def main():
+    try:
+        command = sys.argv[1]
+    except IndexError:
+        print("Accepted commands are: < read >, < link >, < clean >")
+        sys.exit(1)
+
+    original_files = ["products1.txt", "products2.txt", "products3.txt"]
+    
+    
+    if command == "read":
+        read_files(original_files)
+
+    elif command == "link":
+        concatenate_files(original_files)
+    
+    elif command == "clean":
+        clean_dataset(original_files)
+    
+    else:
+        sys.exit(1)
+
+
+
+if __name__ == "__main__":
+    main()
